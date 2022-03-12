f59c4c29d7b522786beace649eba85dac2669a25
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-03-12 17:09:21 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Ball {
+
+    public static void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Kolobok sieden");
+        } else {
+            System.out.println("Kolobok ushel");
+        }
+    }
+}
