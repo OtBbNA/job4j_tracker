423b42cffb3bd06aaa469c3f43a2245afdc861bc
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-03-14 01:00:59 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another){
+        this.load = another.load + this.load;
+        another.load = 0;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+}
