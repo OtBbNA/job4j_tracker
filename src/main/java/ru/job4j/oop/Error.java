cb037acb521ac6c9cbeb93dae6fd00e2c636ee60
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-03-13 18:41:48 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.message= message;
+        this.status = status;
+    }
+
+    public void output(){
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+
+    public static void main(String[] args) {
+    Error error = new Error();
+    error.output();
+    Error error1 = new Error(true, 2, "Play");
+    error1.output();
+    Error error2 = new Error(true, 1, "Off");
+    error2.output();
+    Error error3 = new Error(false, 0, "DeBug");
+    error3.output();
+    }
+}
+
