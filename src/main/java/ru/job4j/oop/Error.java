def57171a8fdd7130c0ac89b967b51421554d1a7
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-03-15 01:07:49 +0300

    1.7. Конструктор [#173889 #274797]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -22,14 +22,14 @@ public class Error {
     }
 
     public static void main(String[] args) {
-    Error error = new Error();
-    error.output();
-    Error error1 = new Error(true, 2, "Play");
-    error1.output();
-    Error error2 = new Error(true, 1, "Off");
-    error2.output();
-    Error error3 = new Error(false, 0, "DeBug");
-    error3.output();
+        Error error = new Error();
+        error.output();
+        Error error1 = new Error(true, 2, "Play");
+        error1.output();
+        Error error2 = new Error(true, 1, "Off");
+        error2.output();
+        Error error3 = new Error(false, 0, "DeBug");
+        error3.output();
      }
 }
 
