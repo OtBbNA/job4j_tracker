c561fd046d32430dd662883b59465194206f2bf2
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-03-15 01:13:11 +0300

    1.7. Конструктор [#173889 #274797]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,7 +8,7 @@ public class Battery {
         this.load = load;
     }
 
-    public void exchange(Battery another){
+    public void exchange(Battery another) {
         this.load = another.load + this.load;
         another.load = 0;
     }
