cf899b25047e41c7a9c99f5a64c13bab78eb2ea4
REDDEVIL <leo.de@mail.ru>
2022-03-31 22:32:39 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743 #282241]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -91,9 +91,6 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
         Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
         new StartUI().init(scanner, tracker);
