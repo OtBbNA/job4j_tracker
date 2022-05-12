9bdcf7203d56ff4c9c986df20eb909736509476a
REDDEVIL <leo.de@mail.ru>
2022-05-12 15:19:16 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("tosha.kostylev.97@mail.ru", "Kostylev Anton");
+        for (String key: hashMap.keySet()) {
+            System.out.println(key + " = " + hashMap.get(key));
+        }
+    }
+}
