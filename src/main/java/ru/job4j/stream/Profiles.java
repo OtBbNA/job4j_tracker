74b7956f6135a018a6eabc20d2db303ea6126d60
REDDEVIL <leo.de@mail.ru>
2022-05-30 17:02:10 +0300

    2. Список адресов [#110062]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(adr -> adr.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(adr -> adr.getAddress())
+                .collect(Collectors.toList());
     }
 }
