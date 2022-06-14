e64594acd6d49fb2889d476e91109a5e43f4b89f
REDDEVIL <leo.de@mail.ru>
2022-06-15 01:03:54 +0300

    2. Аттестация [#49777 #312550]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -31,7 +31,10 @@ public class Analyze {
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream
                 .flatMap(s -> s.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new, Collectors.averagingDouble(t -> t.getScore())))
+                .collect(Collectors.groupingBy(
+                        Subject::getName,
+                        LinkedHashMap::new,
+                        Collectors.averagingDouble(t -> t.getScore())))
                 .entrySet()
                 .stream()
                 .map(s -> new Tuple(s.getKey().toString(), s.getValue()))
@@ -52,7 +55,10 @@ public class Analyze {
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream
                 .flatMap(s -> s.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new, Collectors.summingDouble(t -> t.getScore())))
+                .collect(Collectors.groupingBy(
+                        Subject::getName,
+                        LinkedHashMap::new,
+                        Collectors.summingDouble(t -> t.getScore())))
                 .entrySet()
                 .stream()
                 .map(s -> new Tuple(s.getKey().toString(), s.getValue()))
