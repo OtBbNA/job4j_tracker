dabe4d160f61cc8f0b675694cd7adac9f135be0b
REDDEVIL <leo.de@mail.ru>
2022-08-25 02:03:17 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #335824]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -11,7 +11,7 @@ public class PasswordValidator {
             throw new IllegalArgumentException();
         }
         if (passLength < 8 || passLength > 32) {
-            return "Password length must be between 8 and 32 characters";
+            throw new IllegalArgumentException("Password length must be between 8 and 32 characters");
         }
         boolean passDigit = false;
         boolean passLowerCase = false;
@@ -37,23 +37,23 @@ public class PasswordValidator {
             }
         }
         if (!passDigit) {
-            return "Password must contain numbers";
+            throw new IllegalArgumentException("Password must contain numbers");
         }
         if (!passLowerCase) {
-            return "Password must contain lower characters";
+            throw new IllegalArgumentException("Password must contain lower characters");
         }
         if (!passUpperCase) {
-            return "Password must contain upper characters";
+            throw new IllegalArgumentException("Password must contain upper characters");
         }
         if (!passSpecSymbol) {
-            return "Password must contain at least one special character:"
-                    + "! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~";
+            throw new IllegalArgumentException("Password must contain at least one special character:"
+                    + "! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~");
         }
         String lowerCharPass = password.toLowerCase(Locale.ROOT);
         if (lowerCharPass.contains("qwerty") || lowerCharPass.contains("12345")
                 || lowerCharPass.contains("password") || lowerCharPass.contains("admin")
                 || lowerCharPass.contains("user")) {
-            return ("The password must not contain the substrings "
+            throw new IllegalArgumentException("The password must not contain the substrings "
                     + "\"12345\", \"qwerty\", \"admin\" and etc. Take care "
                     + "of the security of your account!");
         }
--- src/test/java/ru/job4j/early/PasswordValidatorTest.java
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -17,71 +17,87 @@ public class PasswordValidatorTest {
 
     @Test
     public void whenPasswordIsShorterThan8Char() {
-        String password = "Haru6";
-        String expected = "Password length must be between 8 and 32 characters";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("Haru6");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password length must be between 8 and 32 characters");
     }
 
     @Test
     public void whenPasswordIsMoreThan32Char() {
-        String password = "1111111111111111-Haru6aTop-1111111111111111";
-        String expected = "Password length must be between 8 and 32 characters";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("1111111111111111-Haru6aTop-1111111111111111");
+                });
+
     }
 
     @Test
     public void whenPasswordDnotContainsLowercaseChar() {
-        String password = "11-HARU6ATOP-11";
-        String expected = "Password must contain lower characters";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("11-HARU6ATOP-11");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must contain lower characters");
     }
 
     @Test
     public void whenPasswordDnotContainsUppercaseChar() {
-        String password = "11-haru6atop-11";
-        String expected = "Password must contain upper characters";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("11-haru6atop-11");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must contain upper characters");
     }
 
     @Test
     public void whenPasswordDnotContainsDigitChar() {
-        String password = "-HarubaTop-";
-        String expected = "Password must contain numbers";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("-HarubaTop-");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must contain numbers");
     }
 
     @Test
     public void whenPasswordDnotContainsSpecSymbols() {
-        String password = "11Haru6aTop11";
-        String expected = "Password must contain at least one special character:"
-                + "! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("11Haru6aTop11");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must contain at least one special character:"
+                + "! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~");
     }
 
     @Test
     public void whenPasswordContainsQWERTY() {
-        String password = "Haru6aTop_qWeRtY";
-        String expected = "The password must not contain the substrings "
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("Haru6aTop_qWeRtY");
+                });
+        assertThat(exception.getMessage()).isEqualTo("The password must not contain the substrings "
                 + "\"12345\", \"qwerty\", \"admin\" and etc. Take care "
-                + "of the security of your account!";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+                + "of the security of your account!");
     }
 
     @Test
     public void whenPasswordContains12345() {
-        String password = "Haru6aTop_12345";
-        String expected = "The password must not contain the substrings "
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("Haru6aTop_12345");
+                });
+        assertThat(exception.getMessage()).isEqualTo("The password must not contain the substrings "
                 + "\"12345\", \"qwerty\", \"admin\" and etc. Take care "
-                + "of the security of your account!";
-        String result = PasswordValidator.validate(password);
-        assertThat(result).isEqualTo(expected);
+                + "of the security of your account!");
     }
 
     @Test
