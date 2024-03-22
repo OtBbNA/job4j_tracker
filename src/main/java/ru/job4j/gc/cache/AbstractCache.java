4babb7c8ec51ba445d429ad7d57c6ab5f93469b9
OtBbNA <tosha.kostylev.97@mail.ru>
2024-03-22 04:02:55 +0300

    1. Реализация кеша на SoftReference [#1592 #486977]

--- src/main/java/ru/job4j/gc/cache/AbstractCache.java
+++ src/main/java/ru/job4j/gc/cache/AbstractCache.java
@@ -9,13 +9,16 @@ public abstract class AbstractCache<K, V> {
     private final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public final void put(K key, V value) {
-        cache.putIfAbsent(key, new SoftReference<V>(value));
+        cache.put(key, new SoftReference<>(value));
     }
 
     public final V get(K key) {
-        V s = load(key);
-        put(key, s);
-        return cache.get(key).get();
+        V rsl = cache.getOrDefault(key, new SoftReference<>(null)).get();
+        if (rsl == null) {
+            rsl = load(key);
+            put(key, rsl);
+        }
+        return rsl;
     }
 
     protected abstract V load(K key);
