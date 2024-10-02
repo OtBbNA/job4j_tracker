2dc6db66bee20ac21a039d86495f187680f862bd
OtBbNA <tosha.kostylev.97@mail.ru>
2024-10-02 20:43:32 +0300

    1. Конфигурация Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,20 +1,28 @@
 package ru.job4j.tracker;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Entity
+@Table(name = "items")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item implements Comparable<Item> {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
-    public Item() {
-    }
 
     public Item(String name) {
         this.name = name;
@@ -23,21 +31,6 @@ public class Item implements Comparable<Item> {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public Item(int id, String name, LocalDateTime created) {
-        this.id = id;
-        this.name = name;
-        this.created = created.withNano(0);
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id = " + id
-                + ", name = '" + name + '\''
-                + ", created: " + created.format(FORMATTER)
-                + '}';
     }
 
     @Override
